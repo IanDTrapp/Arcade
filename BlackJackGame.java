23d6a6ebe21af085808de29b2e0c80df611cc017
JACOB SWINNEY <swinney@nike.cs.uga.edu>
2014-11-10 22:59:10 -0500

    Lots of stuff

--- BlackJackGame.java
+++ BlackJackGame.java
@@ -2,6 +2,7 @@ public class BlackJackGame
 {
     private GUI gui;
     private ArrayList dealerCards, playerCards;
+    private ArrayList dealerCardImages, playerCardImages;
     private Stack deck;
    
     public void playBlackJack(GUI Gui) 
@@ -20,5 +21,31 @@ public class BlackJackGame
 
 	playerCards.add(deck.pop());
 	playerCards.add(deck.pop());
+    }
+    
+    public ArrayList updateDisplayedCards()
+    {
+	int size = dealerCards.size();
+	for(int i = 0; i < size; i++)
+	{
+	    dealerCardImages.removeRange(0, size);
+	    dealerCardImages.add(dealerCards[i].getImage());
+	}
+	int size1 = playerCards.size();
+	for(int j = 0; j < size1; j++)
+	{
+	    playerCardImages.removeRange(0, size1);
+	    playerCardImages.add(playerCards[j].getImage());
+	}
+    }
+    
+    public ArrayList getDealerImages()
+    {
+	return dealerCardImages;
+    }
+
+    public ArrayList getPlayerImages()
+    {
+	return playerCardImages;
     }
 }
