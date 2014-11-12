8092231dad81a8b6f94869f48ad950c2ee4529e6
IAN TRAPP <trapp@nike.cs.uga.edu>
2014-11-12 15:56:50 -0500

    More changeS

--- BlackJackGame.java
+++ BlackJackGame.java
@@ -206,21 +206,21 @@ public class BlackJackGame
 
     public void split(String player)
     {
-	splitDeck1.add(playerCards[0]);
-	splitDeck2.add(playerCards[1]);
+	splitDeck1.add(playerCards.get(0));
+	splitDeck2.add(playerCards.get(1));
     }
 
     public void hitOnSplit(int deckNum) {
 	// 1 - Left Deck   -- 2 - Right Deck
 	Card card9 = (Card)deck.pop();
-	Card card10 = (card)deck.pop();
+	Card card10 = (Card)deck.pop();
 	int temp1 = card9.getValue().getValue();
 	int temp2 = card10.getValue().getValue();
 	
 	if(deckNum == 1){
 	    splitDeck1.add(card9);
 	    splitTotal1 += temp1;
-	    Split1CardImages.add(card=9.getImage());
+	    split1CardImages.add(card9.getImage());
 	    gui.updateCards(split1CardImages, "player");
 	    if(splitTotal1 > 21)
 		{
@@ -231,7 +231,7 @@ public class BlackJackGame
 	if(deckNum == 2) {
 	    splitDeck2.add(card10);
 	    splitTotal2 += temp2;
-	    split2CardImages.add(card10getImage());
+	    split2CardImages.add(card10.getImage());
 	    gui.updateCards(split2CardImages, "player");
 	    if(splitTotal2 > 21)
 		{
